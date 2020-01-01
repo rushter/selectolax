52298598de783b19fdbc90c9e2db5c008dd86c72
Artem Golubin <me@rushter.com>
2020-01-01 20:35:51 +0300

    Update copyrights

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'selectolax'
-copyright = u"2019, Artem Golubin"
+copyright = u"2018-2020, Artem Golubin"
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout

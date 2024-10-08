a56b20af7653945198684b8f26c756c20ed7e24c
Artem Golubin <me@rushter.com>
2024-10-08 20:37:43 +0400

    Bump version: 0.3.21 → 0.3.22

--- setup.py
+++ setup.py
@@ -167,7 +167,7 @@ def make_extensions():
 
 setup(
     name="selectolax",
-    version='0.3.21',
+    version='0.3.22',
     description="Fast HTML5 parser with CSS selectors.",
     long_description=readme,
     author="Artem Golubin",

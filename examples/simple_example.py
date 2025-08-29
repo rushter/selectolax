8f8b0806f5a50c8127d51fe96d8293667a56758e
Artem Golubin <me@rushter.com>
2025-08-29 09:38:24 +0400

    docs: update example script to use LexborHTMLParser

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -1,9 +1,9 @@
-from selectolax.parser import HTMLParser
+from selectolax.lexbor import LexborHTMLParser
 
 html = "<div><p id=p1><p id=p2><p id=p3><a>link</a><p id=p4><p id=p5>text<p id=p6></div>"
 selector = "div > :nth-child(2n+1):not(:has(a))"
 
-for node in HTMLParser(html).css(selector):
+for node in LexborHTMLParser(html).css(selector):
     print(node.attributes, node.text(), node.tag)
     print(node.parent.tag)
     print(node.html)

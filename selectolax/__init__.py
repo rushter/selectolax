16b0e295adc418580d6e6ec4631fd072eb9f4b8f
Artem Golubin <me@rushter.com>
2026-03-06 13:00:58 +0400

    Bump version: 0.4.6 → 0.4.7

--- selectolax/__init__.py
+++ selectolax/__init__.py
@@ -3,6 +3,6 @@
 
 __author__ = """Artem Golubin"""
 __email__ = "me@rushter.com"
-__version__ = "0.4.6"
+__version__ = "0.4.7"
 
 from . import lexbor, modest, parser
--- setup.py
+++ setup.py
@@ -191,7 +191,7 @@ def make_extensions():
 
 setup(
     name="selectolax",
-    version="0.4.6",
+    version="0.4.7",
     description="A fast HTML5 parser with CSS selectors, written in Cython, using Modest and Lexbor engines.",
     long_description=readme,
     author="Artem Golubin",
